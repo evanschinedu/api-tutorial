8ed5722b6357494755fa7e6ad25c2dfb37fd3a31
Anthonynzube <nzubeanthony4@gmail.com>
2021-05-17 18:52:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const Sequelize = require('sequelize');
+const bodyParser = require('body-parser');
+
+const authenticationRouter = require('./src/routes/authentication_route');
+
+require('./src/db/sequelize');
+const db = require('./src/db/sequelize');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(__dirname));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(authenticationRouter);
+
+module.exports = app;
